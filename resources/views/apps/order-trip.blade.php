2d246c086fbed53887621d13dd9f42d79939bbb4
m.azki <ipa4.23azki@gmail.com>
2017-03-24 17:03:46 +0700

    Test Commit
    Re-commit last change

--- resources/views/apps/order-trip.blade.php
+++ resources/views/apps/order-trip.blade.php
@@ -67,7 +67,6 @@
 
       // This example requires the Places library. Include the libraries=places
       // parameter when you first load the API. For example:
-      // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
 
       function person(){
         var btn = document.getElementById("personal");
